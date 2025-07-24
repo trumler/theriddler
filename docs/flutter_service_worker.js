0cfc9d5e969b0ec43e3c70853c09b20dcf6ab809
trumler <matias.kbr@gmail.com>
2025-07-24 12:24:05 +0200

    Opdater web-build til GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6b3d43e75ba6c327230b12bb0f2df8e0",
+const RESOURCES = {"flutter_bootstrap.js": "ac9223721cf9a78e8951a60f42a00e7b",
 "version.json": "7e16d4ebb19a0059b34f89f12465db4f",
 "index.html": "df78020ef32a5f5f67d013807834f640",
 "/": "df78020ef32a5f5f67d013807834f640",
-"main.dart.js": "c42a89e7f0b004ecccd042d294873c91",
+"main.dart.js": "fefdd1a87f425b526b93a39cd03255bb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "28984f0be62c9c3c4075634e7fa0019e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
