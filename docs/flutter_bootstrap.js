869ce0bbda679c0130ecb90908769ec59573c35f
trumler <matias.kbr@gmail.com>
2025-09-03 09:40:35 +0200

    Opdater web-build til GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2320232989"
+    serviceWorkerVersion: "1921931871"
   }
 });
